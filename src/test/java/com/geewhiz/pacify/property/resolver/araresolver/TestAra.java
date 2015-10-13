d95214be9bd42b61946338a7c1049036304b8aec
Sven Oppermann <sven.oppermann@gmail.com>
2015-10-13 09:49:47 +0200

    adjustments for dont show encryped values in debug messages

--- src/test/java/com/geewhiz/pacify/property/resolver/araresolver/TestAra.java
+++ src/test/java/com/geewhiz/pacify/property/resolver/araresolver/TestAra.java
@@ -1,8 +1,14 @@
 package com.geewhiz.pacify.property.resolver.araresolver;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.io.StringReader;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,11 +16,17 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.xmlbeam.XBProjector;
 
+import com.geewhiz.pacify.CreatePropertyFile;
+import com.geewhiz.pacify.CreatePropertyFile.OutputType;
+import com.geewhiz.pacify.managers.PropertyResolveManager;
 import com.geewhiz.pacify.property.resolver.araresolver.model.AraData;
 import com.geewhiz.pacify.property.resolver.araresolver.model.AraData.GenerateTask;
 import com.geewhiz.pacify.property.resolver.araresolver.model.AraData.Variable;
 import com.geewhiz.pacify.property.resolver.araresolver.model.GenerateTaskMixinImpl;
 import com.geewhiz.pacify.property.resolver.araresolver.model.VariableMixinImpl;
+import com.geewhiz.pacify.resolver.PropertyResolver;
+import com.geewhiz.pacify.test.ListAppender;
+import com.geewhiz.pacify.test.TestUtil;
 import com.geewhiz.pacify.utils.LoggingUtils;
 
 /*
@@ -43,20 +55,7 @@ public class TestAra {
         Logger logger = LogManager.getLogger(TestAra.class.getName());
         LoggingUtils.setLogLevel(logger, Level.DEBUG);
 
-        AraPropertyResolver araPropertyResolver = new AraPropertyResolver();
-
-        araPropertyResolver.setInitilized(true);
-        araPropertyResolver.setTarget("server1.example.org");
-        araPropertyResolver.setComponent("Componente_1");
-        araPropertyResolver.setNamespace("/example_namespace");
-        araPropertyResolver.setDecodePasswordWithBase64(true);
-
-        XBProjector xbProjector = new XBProjector();
-        xbProjector.mixins().addProjectionMixin(Variable.class, new VariableMixinImpl("=>", "UTF-8", Boolean.TRUE));
-        xbProjector.mixins().addProjectionMixin(GenerateTask.class, new GenerateTaskMixinImpl("=>"));
-
-        AraData araData = xbProjector.io().file("target/test-classes/example_ara_output_cddata.xml").read(AraData.class);
-        araPropertyResolver.setAraData(araData);
+        AraPropertyResolver araPropertyResolver = createAraPropertyResolver();
 
         Set<String> properties = null;
 
@@ -79,4 +78,70 @@ public class TestAra {
         Assert.assertEquals("Wrong property size count.", 1, properties.size());
         Assert.assertEquals("Componente_2_another_namespace_foobar1_value", araPropertyResolver.getPropertyValue("foobar1"));
     }
+
+    @Test
+    public void callTest() throws IOException {
+        Logger logger = LogManager.getLogger(TestAra.class.getName());
+        LoggingUtils.setLogLevel(logger, Level.DEBUG);
+
+        AraPropertyResolver araPropertyResolver = createAraPropertyResolver();
+        PropertyResolveManager prm = createPropertyResolveManager(araPropertyResolver);
+        CreatePropertyFile createPropertyFile = new CreatePropertyFile(prm);
+
+        createPropertyFile.setOutputType(OutputType.Stdout);
+        createPropertyFile.setOutputEncoding("utf-8");
+        createPropertyFile.setOutputPrefix("");
+        createPropertyFile.setFilemode("400");
+
+        araPropertyResolver.setComponent("Componente_1");
+        araPropertyResolver.setNamespace("/example_namespace");
+
+        ListAppender listAppender = TestUtil.addListAppenderToLogger();
+        PrintStream oldStdOut = System.out;
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        try {
+            createPropertyFile.write();
+        }
+        finally {
+            System.setOut(oldStdOut);
+            outContent.close();
+        }
+
+        // Test the DEBUG LOG Messages, we should not see the encrypted value
+        Assert.assertEquals("We expect log lines:", 6, listAppender.getLogMessages().size());
+        Assert.assertEquals("Password should be encrypted in debug message.", "       Resolved property [foobar3] to value [**********]", listAppender
+                .getLogMessages().get(4));
+
+        // Test the STDOUT there we should see the encrypted value
+        List<String> outputLines = IOUtils.readLines(new StringReader(outContent.toString()));
+        Assert.assertEquals("We expect the decoded value", "foobar3=encryptedPassword", outputLines.get(2));
+    }
+
+    private AraPropertyResolver createAraPropertyResolver() throws IOException {
+        AraPropertyResolver araPropertyResolver = new AraPropertyResolver();
+
+        araPropertyResolver.setInitilized(true);
+        araPropertyResolver.setTarget("server1.example.org");
+        araPropertyResolver.setDecodePasswordWithBase64(true);
+        araPropertyResolver.setBeginToken("@");
+        araPropertyResolver.setEndToken("@");
+
+        XBProjector xbProjector = new XBProjector();
+        xbProjector.mixins().addProjectionMixin(Variable.class, new VariableMixinImpl("=>", "UTF-8", Boolean.TRUE));
+        xbProjector.mixins().addProjectionMixin(GenerateTask.class, new GenerateTaskMixinImpl("=>"));
+
+        AraData araData = xbProjector.io().file("target/test-classes/example_ara_output_cddata.xml").read(AraData.class);
+        araPropertyResolver.setAraData(araData);
+        return araPropertyResolver;
+    }
+
+    private PropertyResolveManager createPropertyResolveManager(AraPropertyResolver apr) {
+        Set<PropertyResolver> propertyResolverList = new TreeSet<PropertyResolver>();
+        propertyResolverList.add(apr);
+        PropertyResolveManager prm = new PropertyResolveManager(propertyResolverList);
+        return prm;
+    }
+
 }
